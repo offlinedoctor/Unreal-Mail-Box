64cc05b01a49eb7508867f32484c5d1d97765158
Tim N <nguyentimwork@gmail.com>
2022-04-30 20:44:28 +1000

    Fix structure for mailbox sending.
    Ensure ue4 backend now sends the properly formatted JSON to React

--- ReactFrontend/react-mailbox/src/App.js
+++ ReactFrontend/react-mailbox/src/App.js
@@ -8,27 +8,50 @@ import {Input} from 'antd';
 const  {TextArea} = Input;
 
 var ArrayList = 
-[
-	{
-		username: "hello",
-		chat: "hello",
-		timestamp: "hello"
-	},
-	{
-		username: "hello",
-		chat: "hello",
-		timestamp: "hello"
-	}
-];
+{
+	"List":
+	[
+		{
+			username: "hello1",
+			chat: "hello1",
+			timestamp: "hello1"
+		},
+		{
+			username: "hello232",
+			chat: "hello2",
+			timestamp: "hello2"
+		}
+	]
+};
 
 window.ue.interface.UpdateArrayList = function(JSON_PayLoad)
 {
-	console.log(JSON_PayLoad);
 	ArrayList = JSON_PayLoad;
+	console.log(ArrayList);
+	window.AppComponent.UpdateStateArrayList();
 }
 
 class App extends React.Component
 {
+	constructor()
+	{
+		super();
+		
+		this.state = 
+		{	
+			StateArrayList: ArrayList,
+		}
+	
+		window.AppComponent = this;
+		this.UpdateStateArrayList = this.UpdateStateArrayList.bind(this);
+	}
+	
+	UpdateStateArrayList()
+	{
+		this.setState({StateArrayList: ArrayList});
+		console.log(this.StateArrayList);
+	}
+	
 	render()
 	{
 		return(
@@ -39,7 +62,7 @@ class App extends React.Component
 				<div style={{display: "flex", flexDirection: "column", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)"}}>
 					<div style={{height: "500px", overflowY: "auto", overflowX: "hidden"}}>
 					{
-						ArrayList.map(eachIteration =>
+						this.state.StateArrayList.List.map(eachIteration =>
 							<EachChatBox username={eachIteration.username} chat={eachIteration.chat} timestamp = {eachIteration.timestamp}/>
 						)
 					}
