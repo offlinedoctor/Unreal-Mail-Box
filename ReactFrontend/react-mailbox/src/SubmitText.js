049254c80dbff28404895ef76fb3c96a0909c4e4
Tim N <nguyentimwork@gmail.com>
2022-05-02 20:31:21 +1000

    Implement replicated chat between server & clients.

--- ReactFrontend/react-mailbox/src/SubmitText.js
+++ ReactFrontend/react-mailbox/src/SubmitText.js
@@ -8,11 +8,29 @@ const { TextArea } = Input;
 
 class SubmitText extends React.Component
 {
+	constructor()
+	{
+		super();
+		
+		this.keyPress = this.keyPress.bind(this);
+	}
+	
+	keyPress(event)
+	{
+		console.log(event.key);
+		
+		if (event.key === "Enter")
+		{
+			console.log(document.getElementById("InputText").value);
+			window.ue4("AddToMailboxArray", document.getElementById("InputText").value);
+		}
+	}
+	
 	render()
 	{
 		return(
 			<div style={{display: "flex", flexDirection: "row", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)"}}>
-				<TextArea rows={2} style={{resize: "none"}}/>
+				<TextArea rows={2} id="InputText" onKeyDown={this.keyPress} style={{resize: "none"}}/>
 				<Button type="primary" style={{height: "auto", width: "15%"}} icon=<CaretRightOutlined/>></Button>
 			</div>
 		);
