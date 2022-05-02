48c2fa65f1c85012c2be1e52e545134ceab67390
Tim N <nguyentimwork@gmail.com>
2022-05-03 00:45:30 +1000

    Front-end polish.
    Scrollbar cleanup.

--- ReactFrontend/react-mailbox/src/App.js
+++ ReactFrontend/react-mailbox/src/App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import SubmitText from './SubmitText.js';
 import SideBar from './SideBar.js';
 import EachChatBox from './EachChatBox.js';
+import StyleCSS from './StyleCSS.css';
 import {Button} from 'antd';
 import {Input} from 'antd';
 const  {TextArea} = Input;
+
 
 var ArrayList = 
 {
@@ -52,15 +54,21 @@ class App extends React.Component
 		console.log(this.StateArrayList);
 	}
 	
+	componentDidMount()
+	{
+		var objDiv = document.getElementById("customSlider");
+		objDiv.scrollTop = objDiv.scrollHeight;
+	}
+	
 	render()
 	{
 		return(
-			<div style={{display: "flex", flexDirection: "row", height: "100%", width: "100%", padding: "25px"}}>
+			<div style={{display: "flex", flexDirection: "row", height: "100%", width: "100%", padding: "25px", border: "25px solid rgba(1,1,1,0.5)"}}>
 				<div style={{marginRight: "20px"}}>
 					<SideBar/>
 				</div>
-				<div style={{display: "flex", flexDirection: "column", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)", width: "100%", backgroundColor: "rgba(238,238,238,0.95)", borderRadius: "25px"}}>
-					<div style={{overflowY: "auto", overflowX: "hidden", height: "100%"}}>
+				<div style={{display: "flex", flexDirection: "column", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)", width: "100%", backgroundColor: "rgba(238,238,238,0.95)", borderTopLeftRadius: "25px", borderTopRightRadius: "25px", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px", overflow: "hidden"}}>
+					<div  id="customSlider" style={{overflowY: "auto", overflowX: "hidden", height: "100%"}}>
 					{
 						this.state.StateArrayList.List.map(eachIteration =>
 							<EachChatBox username={eachIteration.username} chat={eachIteration.chat} timestamp = {eachIteration.timestamp}/>
--- ReactFrontend/react-mailbox/src/SideBar.js
+++ ReactFrontend/react-mailbox/src/SideBar.js
@@ -49,7 +49,7 @@ class SideBar extends React.Component
 	render()
 	{
 		return(
-			<div style={{padding: "25px", backgroundColor: "rgba(1,1,1,0.8)", borderRadius: "15px", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)"}}>
+			<div style={{padding: "25px", backgroundColor: "rgba(1,1,1,0.75)", borderRadius: "15px", boxShadow: "5px 5px 10px 0px rgba(0,0,0,0.2)"}}>
 				<div style={{display: "flex", flexDirection: "column", paddingBottom: "25px", borderBottom: "1px solid", alignItems: "center"}}>
 					<h2 style={{color: "white"}}> Session </h2>
 					{
@@ -66,7 +66,7 @@ class SideBar extends React.Component
 						<Tooltip title="Who is Hosting?" placement="right">
 							<Button type="primary" onClick={this.ShowHost} shape="square" icon={<CrownOutlined />}/>
 						</Tooltip>
-						<Tooltip title="Ring Bell" placement="right">
+						<Tooltip title="Ring Bell"  placement="right">
 							<Button type="primary" onClick={this.NotificationSound} shape="square" icon={<AlertOutlined />}/>
 						</Tooltip>
 					</div>
